97f9903490eb486bb73ee25780365a6fb7179b3a
Mark Evans <mark.evans@sedna.com>
2024-05-07 19:49:53 +0100

    weren't supposed to commit js files

--- packages/appipelago-svelte/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { SvelteComponent } from "svelte";
-declare const createSvelteApp: (Component: any, opts?: {
-    callbackParams?: Record<string, (...args: any[]) => Record<string, any>>;
-}) => JsApp<{
-    app: SvelteComponent;
-}>;
-export default createSvelteApp;
