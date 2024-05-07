49cb4309132038648793f26ad344af9d16fcd1ba
Mark Evans <mark.evans@sedna.com>
2024-05-07 16:29:46 +0100

    correct vite config

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [dts()],
+  plugins: [dts({ include: ["src"] })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
