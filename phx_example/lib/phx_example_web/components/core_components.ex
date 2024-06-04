1dc1f9ced48105c612cfaf3cda4ae34d3817c816
Mark Evans <mark.evans@sedna.com>
2024-06-04 16:52:47 +0100

    Macros doesn't exist any more

--- phx_example/lib/phx_example_web/components/core_components.ex
+++ phx_example/lib/phx_example_web/components/core_components.ex
@@ -674,10 +674,4 @@ defmodule PhxExampleWeb.CoreComponents do
   def translate_errors(errors, field) when is_list(errors) do
     for {^field, {msg, opts}} <- errors, do: translate_error({msg, opts})
   end
-
-  import Komodo.Macros
-
-  defjscomponent(:react_map)
-  defjscomponent(:svelte_map)
-  defjscomponent(:vue_map)
 end
